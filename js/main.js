a92af523f6a1b783fe8c009eff2c1d9ef3c52829
taccumei <dangngoctram1903@gmail.com>
2024-08-04 12:51:22 -0700

    about me, skills function

--- js/main.js
+++ js/main.js
@@ -14,3 +14,14 @@ overlay.addEventListener("click", () => {
   menuButton.classList.toggle("open");
   overlay.classList.toggle("show");
 });
+
+function setCatURL(newUrl) {
+  var url = newUrl;
+  return function () {
+    const catPic = document.querySelector("#catpic");
+    catPic.setAttribute('src', url);
+    catPic.className = "cat__pic";
+  }
+}
+  
+document.getElementById('caturl').addEventListener('mouseover', setCatURL('img/cats.JPG'));
